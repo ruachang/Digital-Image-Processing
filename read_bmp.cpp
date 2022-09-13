9d2831818c931d6e9b481f45853bbf215d457b13
ruachang <lcfatego@gmail.com>
2022-09-13 15:38:07 +0800

    复健:使用C++编译

--- read_bmp.cpp
+++ read_bmp.cpp
@@ -1,3 +1,21 @@
 /*
-
+读入bmp文件: 按字节读取
+读取的顺序就是存储的顺序, 高位对应前面, 低位对应后面
 */
+
+#include <iostream>
+#include <vector>
+#include <string>
+
+using namespace std;
+
+int main()
+{
+    vector<string> msg {"Hello", "C++", "World", "from", "VS Code", "and the C++ extension!"};
+    
+    for (const string& word : msg)
+    {
+        cout << word << " ";
+    }
+    cout << endl;
+}
